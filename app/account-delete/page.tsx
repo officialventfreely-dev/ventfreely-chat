2837e6d182e502c18ed7497f1d72eb1a2a5346a9
officialventfreely-dev <official.ventfreely@gmail.com>
2026-01-19 23:09:34 +0200

    account-delete

---
+++ app/account-delete/page.tsx
@@ -0,0 +1,21 @@
+export default function AccountDeletePage() {
+  return (
+    <main style={{ padding: 24, maxWidth: 640, margin: "0 auto" }}>
+      <h1>Delete your Ventfreely account</h1>
+
+      <p>
+        You can request deletion of your Ventfreely account and all associated data
+        by sending an email to:
+      </p>
+
+      <p style={{ fontWeight: "bold", marginTop: 12 }}>
+        official.ventfreely@gmail.com
+      </p>
+
+      <p style={{ marginTop: 16 }}>
+        Please include the email address you used to sign up.
+        Your account and data will be deleted within 7 days.
+      </p>
+    </main>
+  );
+}
